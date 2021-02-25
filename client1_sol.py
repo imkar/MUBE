097e70b0ff03e5872aba401cc40db2cdbf1f59a9
imkar <35222613+imkar@users.noreply.github.com>
2021-02-25 06:15:58 +0300

    Update client1_sol.py

--- client1_sol.py
+++ client1_sol.py
@@ -1,13 +1,13 @@
 from socket import *
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect(('192.168.1.51', 5000))
+s.connect((IP_ADDR, 5000))
 print("client is connected")
 
 print("what do you want to send!")
 inp = input()
 mesg = inp.encode('utf-8')
-s.sendto(mesg,('192.168.1.51',5000))
+s.sendto(mesg,(IP_ADDR,5000))
 
 data = s.recv(1024)
 print('Received', data)
